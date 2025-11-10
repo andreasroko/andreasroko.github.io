526fc5be7f2da9d653b9e76d446ac9e587a8f80b
Andreas Rokopanos <arokopanos@deloitte.gr>
2025-11-10 17:07:56 +0200

    Add correct oath for assets

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/andreasroko.github.io',
-  assetPrefix: '/andreasroko.github.io/',
+  assetPrefix: '/andreasroko.github.io/public',
 };
 
 export default nextConfig;
